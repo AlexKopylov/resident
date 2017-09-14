86508a1e0ef4bf14b45211c550dd260a5b3dd8bc
Alex Kopylov <alex.kopylov.08@gmail.com>
2017-09-14 10:12:46 +0300

    add slick_slider

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,32 @@
 $(function() {
 
-	$(".param__list")
+	// $(".param__list");
 
+	$(document).ready(function(){
+		$(".owl-carousel").owlCarousel({
+			// loop:true,
+			// margin:10,
+			// dots:false,
+			// nav:true,
+			// responsive:{
+			// 	0:{
+			// 		items:1
+			// 	},
+			// 	600:{
+			// 		items:3
+			// 	},
+			// 	1000:{
+			// 		items:5
+			// 	}
+			// }
+			loop: true,
+			autoplay: true,
+			items: 1,
+			nav: true,
+			autoplayHoverPause: true,
+			animateOut: 'slideOutUp',
+			animateIn: 'slideInUp'
+
+		});
+	});
 });
